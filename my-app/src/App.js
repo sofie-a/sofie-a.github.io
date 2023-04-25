b748848557de80bfc45d94e0edf5c5340615ad10
Ann-Sofie <annsofiexeriksson@gmail.com>
2023-04-25 23:42:01 +0200

    Started base for portfolio website

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+// import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Ann-Sofie Eriksson
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
